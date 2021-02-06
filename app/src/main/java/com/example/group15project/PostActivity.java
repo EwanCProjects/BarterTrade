4087a933f613588cb0a3bfec0d592fcb8ef0b8cc
Isaac Xu <isaac.xu@dal.ca>
2021-02-06 17:54:32 -0400

    Completed JUnit Tests. Only AT-I and AT V not captured in JUnit tests due to their nature (AT-I is making the page and AT-V is adding info to Firebase DB)

--- app/src/main/java/com/example/group15project/PostActivity.java
+++ app/src/main/java/com/example/group15project/PostActivity.java
@@ -3,6 +3,7 @@ package com.example.group15project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class PostActivity extends AppCompatActivity {
@@ -11,5 +12,32 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+    }
+
+    protected String getPostTitle() {
+        EditText userName = findViewById(R.id.titleTextField);
+        return userName.getText().toString().trim();
+    }
+
+    protected String getPostDescription() {
+        EditText emailAddress = findViewById(R.id.descriptionTextField);
+        return emailAddress.getText().toString().trim();
+    }
+
+    protected String getPostCategory() {
+        EditText emailAddress = findViewById(R.id.categoryTextField);
+        return emailAddress.getText().toString().trim();
+    }
+
+    protected boolean isEmptyTitle(String title) {
+        return title.isEmpty();
+    }
+
+    protected boolean isEmptyDescription(String description) {
+        return description.isEmpty();
+    }
+
+    protected boolean isEmptyCategory(String category) {
+        return category.isEmpty();
     }
 }
--- app/src/test/java/com/example/group15project/JUnitTestPost.java
+++ app/src/test/java/com/example/group15project/JUnitTestPost.java
@@ -1,5 +1,8 @@
 package com.example.group15project;
 
+import android.app.Activity;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +13,31 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class JUnitTestPost {
+    static PostActivity postActivity;
+
+    @BeforeClass
+    public static void setup() {
+        postActivity = new PostActivity();
+    }
+
+    /*** AT-II**/
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void checkIfTitleIsEmpty() {
+        assertTrue(postActivity.isEmptyTitle(""));
+        assertFalse(postActivity.isEmptyTitle("xyz$56"));
+    }
+
+    /*** AT-III**/
+    @Test
+    public void checkIfCategoryIsEmpty() {
+        assertTrue(postActivity.isEmptyCategory(""));
+        assertFalse(postActivity.isEmptyCategory("xyz$56"));
+    }
+
+    /*** AT-IV**/
+    @Test
+    public void checkIfDescriptionIsEmpty() {
+        assertTrue(postActivity.isEmptyDescription(""));
+        assertFalse(postActivity.isEmptyDescription("xyz$56"));
     }
 }
