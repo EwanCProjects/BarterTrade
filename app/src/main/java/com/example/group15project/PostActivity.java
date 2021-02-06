6012a2f3c1fe1ab41f494fa6d9340b740c04caae
Isaac Xu <isaac.xu@dal.ca>
2021-02-06 16:37:24 -0400

    Add Post UI and Post Activity

---
+++ app/src/main/java/com/example/group15project/PostActivity.java
@@ -0,0 +1,15 @@
+package com.example.group15project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
--- app/src/test/java/com/example/group15project/JUnitTestPost.java
+++ app/src/test/java/com/example/group15project/JUnitTestPost.java
@@ -1,4 +1,17 @@
 package com.example.group15project;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 public class JUnitTestPost {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
