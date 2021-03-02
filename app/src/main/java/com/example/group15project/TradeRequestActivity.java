a6d9a50675cdfcbfade00f0c5525626525cef737
Alex Goddard <arg@dal.ca>
2021-03-02 16:37:04 -0400

    Updated the layout design, wrote espresso tests, started writing Junit tests and started writing main activity for trade requests [A.G.]

---
+++ app/src/main/java/com/example/group15project/TradeRequestActivity.java
@@ -0,0 +1,20 @@
+package com.example.group15project;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TradeRequestActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trade_request);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/test/java/com/example/group15project/JUnitTestTradeRequest.java
@@ -0,0 +1,19 @@
+package com.example.group15project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class JUnitTestTradeRequest {
+
+    @Test
+    public void isTitleEmpty() {
+
+    }
+
+    @Test
+    public void isDescriptionEmpty() {
+
+    }
+}
