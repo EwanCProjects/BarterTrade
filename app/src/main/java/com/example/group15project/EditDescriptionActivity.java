d5aca96a197826aff4a2b933d7a6b9b765a1b347
AerianaNarbonne <ar888511@dal.ca>
2021-03-05 21:08:05 -0400

    ready to run tests

--- app/src/main/java/com/example/group15project/EditDescriptionActivity.java
+++ app/src/main/java/com/example/group15project/EditDescriptionActivity.java
@@ -9,20 +9,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.core.view.View;
 
-public class TestEditDescriptionActivity extends AppCompatActivity {
+public class EditDescriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_post);
+        setContentView(R.layout.activity_edit_description);
     }
 
     public void buttonClicked(View view){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("editDesc"); // ?
 
-        EditText editText = (EditText) findViewById(R.id.editDesciption);
+        EditText editText = findViewById(R.id.editDesciption);
         String editedText = editText.getText().toString();
         myRef.setValue(editedText);
     }
+
 }
