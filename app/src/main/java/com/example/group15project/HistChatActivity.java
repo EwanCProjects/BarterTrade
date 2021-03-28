0bee67e45abe538395ee55b2d2eec338e7906ee4
Isaac Xu <isaac.xu@dal.ca>
2021-03-28 17:40:39 -0300

    Chat history working (in theory)

--- app/src/main/java/com/example/group15project/HistChatActivity.java
+++ app/src/main/java/com/example/group15project/HistChatActivity.java
@@ -3,15 +3,65 @@ package com.example.group15project;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistChatActivity extends AppCompatActivity implements View.OnClickListener{
+    RecyclerView histChatView;
+    HistChatAdapter histChatAdapter;
 
-    public Conversation currConversation;
+    public static String currUser = HomeActivity.currUser;
+    DatabaseReference realTimeDatabase = HomeActivity.realTimeDatabase;
+    List<Conversation> extractedConversations = new ArrayList<>();
+    List<String> conversationPartners = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_history);
+
+        histChatView = findViewById(R.id.histChatView);
+
+        histChatAdapter = new HistChatAdapter(this, extractedConversations, conversationPartners);
+        histChatView.setAdapter(histChatAdapter);
+        histChatView.setLayoutManager(new LinearLayoutManager(this));
+
+        databaseRead(realTimeDatabase);
+
+    }
+
+    public void databaseRead(DatabaseReference db) {
+        //code for database initialization and grabbing all Posts
+        ValueEventListener chatListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot chatSnapshot : dataSnapshot.child("Conversations").getChildren()) {
+                    Conversation extractedConversation = chatSnapshot.getValue(Conversation.class);
+                    if (extractedConversation.getUser().equals(currUser)) {
+                        extractedConversations.add(extractedConversation);
+                        conversationPartners.add(extractedConversation.getOppositeUser());
+                        histChatAdapter.notifyDataSetChanged();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        };
+        db.addValueEventListener(chatListener);
     }
 
     @Override
--- app/src/main/java/com/example/group15project/HistChatAdapter.java
+++ app/src/main/java/com/example/group15project/HistChatAdapter.java
@@ -1,5 +1,62 @@
 package com.example.group15project;
 
-public class HistChatAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class HistChatAdapter extends RecyclerView.Adapter<HistChatAdapter.HistChatViewHolder> {
     public static Conversation currConversation;
+    List<Conversation> conversations;
+    List<String> chatPartners;
+    Context context;
+
+    public HistChatAdapter(Context ct, List<Conversation> conversations, List<String> chatPartners) {
+        context = ct;
+        this.conversations = conversations;
+        this.chatPartners = chatPartners;
+    }
+
+    @NonNull
+    @Override
+    public HistChatAdapter.HistChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.hist_chat_cardview, parent, false);
+        return new HistChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistChatAdapter.HistChatViewHolder holder, int position) {
+        holder.chatPartner.setText(chatPartners.get(position));
+
+        holder.chatLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(context, HistChatActivity.class);
+            currConversation = conversations.get(position);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return conversations.size();
+    }
+
+    public class HistChatViewHolder extends RecyclerView.ViewHolder {
+        TextView chatPartner;
+        ConstraintLayout chatLayout;
+
+        public HistChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            chatPartner = itemView.findViewById(R.id.chatPartnerName);
+            chatLayout = itemView.findViewById(R.id.chatCardLayout);
+        }
+    }
 }
