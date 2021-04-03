a8a8a3598a4646d0286548901649d5f8707af1af
AerianaNarbonne <ar888511@dal.ca>
2021-04-02 21:22:15 -0300

    SearchBar Done, ATTEMPT 3

--- app/src/main/java/com/example/group15project/HomeActivity.java
+++ app/src/main/java/com/example/group15project/HomeActivity.java
@@ -50,7 +50,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public static DatabaseReference databaseReference;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
